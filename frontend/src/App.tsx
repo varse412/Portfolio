44d95669df4ff20cc3d007379dfe1b0f1dfb5878
Varun Sehgal <varunsehgal412@gmail.com>
2024-03-29 21:25:27 +0530

    changes in styling in main entry files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+const redstyle="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-56"
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1 className="text-3xl font-normal underline bg-red-800">
+      Hello world!
+    </h1>
+    <div className={redstyle}>
+  <div className="shrink-0">
+    <img className="h-12 w-12" src={reactLogo} alt="ChitChat Logo"/>
+  </div>
+  <div>
+    <div className="text-xl font-medium text-black">ChitChat</div>
+    <p className="text-slate-500">You have a new message!</p>
+  </div>
+</div>      
+<button className="dark:md:hover:bg-fuchsia-600 ">
+  Save changes
+</button>
     </>
   )
 }
