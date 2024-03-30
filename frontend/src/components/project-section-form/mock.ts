19d9b68cd28eacbe381e9a6c1300e0ab27e22a42
Varun Sehgal <varunsehgal412@gmail.com>
2024-03-30 23:59:36 +0530

    mock data

---
+++ frontend/src/components/project-section-form/mock.ts
@@ -0,0 +1,109 @@
+// Form contains the following fields
+// for dashboard handling
+const data={
+    "projectName":"fvsfsfsffsfsfsfsfsfsfsfsdffsfsfsdfsfsfs",
+    "developmentType":"full stack " ,
+    "projectDescription":"",
+    "techStack":[{"software": "","Keypoints": ["vsvdsvs","cssdds","","","","",""]}],
+    "projectLink":"https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js",
+    "githubLink": "https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js",
+    "liveLink": "https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js",
+    "picture": "https://picsum.photos/200/300.jpg",
+    "demoVideo": "https://www.youtube.com/watch?v=erLbbextvlY&ab_channel=MrBeast",  
+}
+// for dashboard handling
+
+const profileDetails={
+    "name": "Varun Sehgal",
+    "imgUri": "https://picsum.photos/200/300.jpg",
+    "email": "abcd@gmail.com",
+    "github": "https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js",
+    // "twitter": "",
+    "linkedin": "https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js",
+    "website": "https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js",
+    "bio": "Aliquip et ea minim commodo nisi incididunt id nisi qui do exercitation. Esse excepteur culpa eiusmod sunt do ut occaecat. Ad excepteur laboris cupidatat ex adipisicing nisi. Dolor voluptate est nostrud mollit non laborum voluptate Lorem dolor excepteur fugiat occaecat dolore. Mollit enim velit nulla ullamco amet pariatur commodo.",
+    "location": "vsvsdsdfsdf",
+    "mobile": "9123123432",
+    "resume": "in pdf format",
+    "resumeLink": "http://github.com/",
+    "title": ""
+}
+// for dashboard handling
+
+const displayPictureWithdummy={
+    "imgUri": "http://google.com/",
+    "title":"Consequat occaecat est dolore labore.",
+    "description": "Consectetur elit velit laboris mollit. Consequat culpa cillum officia anim esse tempor qui sit labore. Eiusmod tempor nulla dolore reprehenderit non est do dolore est Lorem nisi mollit est eiusmod. Ipsum deserunt sunt minim eu cillum sit tempor sunt magna laboris ex eiusmod. Incididunt non occaecat occaecat tempor deserunt do dolore nisi eu aliqua exercitation velit amet. Aliqua consequat enim labore velit cupidatat elit laboris commodo mollit."
+}
+
+
+const skillData={
+    "StackName": "",
+    //multiple lines ,
+    "insideStack": ["Ipsum sit consectetur."," aliquip eiusmod","non culpa","Ipsum sit consectetur."," aliquip eiusmod","non culpa"],
+}
+
+const certifications=[
+    {
+        "name":"varuncsfsfs",
+        "image": "sfsffsfs",
+        "certificateLinkToDownload": ""
+    },
+    {
+        "name":"varuncsfsfs",
+        "image": "sfsffsfs",
+        "certificateLinkToDownload": ""
+    },
+]
+const professionalWorkExperiences = [
+    {
+    "companyName": "abcZfwfsfsfsfsfs",
+    "position": "Sde-1",
+    "startDate": "3-6-2023",
+    "endDate": "present",
+    "description": "Cupidatat pariatur ea ex eu voluptate voluptate. Sint consectetur aute eiusmod sit sit reprehenderit et incididunt elit nostrud tempor exercitation. Duis reprehenderit adipisicing veniam minim nisi elit aute minim quis sint ea aute reprehenderit et. Enim velit Lorem proident ad cupidatat. Irure sunt incididunt officia nisi cupidatat nisi officia est. Adipisicing tempor nostrud anim pariatur nulla sit culpa cupidatat duis labore aliquip cupidatat nostrud commodo. Ullamco eu occaecat aute cupidatat irure minim pariatur sint mollit incididunt.",
+    "CompanyImage": ""
+    },
+    {
+        "companyName": "abcZfwfsfsfsfsfs",
+        "position": "Sde-1",
+        "startDate": "3-6-2023",
+        "endDate": "present",
+        "description": "Cupidatat pariatur ea ex eu voluptate voluptate. Sint consectetur aute eiusmod sit sit reprehenderit et incididunt elit nostrud tempor exercitation. Duis reprehenderit adipisicing veniam minim nisi elit aute minim quis sint ea aute reprehenderit et. Enim velit Lorem proident ad cupidatat. Irure sunt incididunt officia nisi cupidatat nisi officia est. Adipisicing tempor nostrud anim pariatur nulla sit culpa cupidatat duis labore aliquip cupidatat nostrud commodo. Ullamco eu occaecat aute cupidatat irure minim pariatur sint mollit incididunt.",
+        "CompanyImage": ""
+        },
+        {
+            "companyName": "abcZfwfsfsfsfsfs",
+            "position": "Sde-1",
+            "startDate": "3-6-2023",
+            "endDate": "present",
+            "description": "Cupidatat pariatur ea ex eu voluptate voluptate. Sint consectetur aute eiusmod sit sit reprehenderit et incididunt elit nostrud tempor exercitation. Duis reprehenderit adipisicing veniam minim nisi elit aute minim quis sint ea aute reprehenderit et. Enim velit Lorem proident ad cupidatat. Irure sunt incididunt officia nisi cupidatat nisi officia est. Adipisicing tempor nostrud anim pariatur nulla sit culpa cupidatat duis labore aliquip cupidatat nostrud commodo. Ullamco eu occaecat aute cupidatat irure minim pariatur sint mollit incididunt.",
+            "CompanyImage": ""
+            },
+]
+const education=[{
+    "schoolName": "abcZfwfsfsfsfsfs",
+    "degree": "Sde-1",
+    "startDate": "3-6-2023",
+    "endDate": "present",
+    "description": "Cupidatat pariatur ea ex eu voluptate voluptate. Sint consectetur aute eiusmod sit sit reprehenderit et incididunt elit nostrud tempor exercitation. Duis reprehenderit adipisicing veniam minim nisi elit aute minim quis sint ea aute reprehenderit et. Enim velit Lorem proident ad cupidatat. Irure sunt incididunt officia nisi cupidatat nisi officia est. Adipisicing tempor nostrud anim pariatur nulla sit culpa cupidatat duis labore aliquip cupidatat nostrud commodo. Ullamco eu occaecat aute cupidatat irure minim pariatur sint mollit incididunt.",
+    "CompanyImage": ""
+    },
+    {
+        "schoolName": "abcZfwfsfsfsfsfs",
+        "degree": "Sde-1",
+        "startDate": "3-6-2023",
+        "endDate": "present",
+        "description": "Cupidatat pariatur ea ex eu voluptate voluptate. Sint consectetur aute eiusmod sit sit reprehenderit et incididunt elit nostrud tempor exercitation. Duis reprehenderit adipisicing veniam minim nisi elit aute minim quis sint ea aute reprehenderit et. Enim velit Lorem proident ad cupidatat. Irure sunt incididunt officia nisi cupidatat nisi officia est. Adipisicing tempor nostrud anim pariatur nulla sit culpa cupidatat duis labore aliquip cupidatat nostrud commodo. Ullamco eu occaecat aute cupidatat irure minim pariatur sint mollit incididunt.",
+        "CompanyImage": ""
+        },
+        {
+            "schoolName": "abcZfwfsfsfsfsfs",
+            "degree": "Sde-1",
+            "startDate": "3-6-2023",
+            "endDate": "present",
+            "description": "Cupidatat pariatur ea ex eu voluptate voluptate. Sint consectetur aute eiusmod sit sit reprehenderit et incididunt elit nostrud tempor exercitation. Duis reprehenderit adipisicing veniam minim nisi elit aute minim quis sint ea aute reprehenderit et. Enim velit Lorem proident ad cupidatat. Irure sunt incididunt officia nisi cupidatat nisi officia est. Adipisicing tempor nostrud anim pariatur nulla sit culpa cupidatat duis labore aliquip cupidatat nostrud commodo. Ullamco eu occaecat aute cupidatat irure minim pariatur sint mollit incididunt.",
+            "CompanyImage": ""
+            },
+]
+export {data,profileDetails,displayPictureWithdummy,skillData,certifications,professionalWorkExperiences}
