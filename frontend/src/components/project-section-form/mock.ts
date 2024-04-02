849bea86d52d51a5d00cb08d6c770035b72f9635
Varun Sehgal <varunsehgal412@gmail.com>
2024-04-02 23:09:17 +0530

    mock data for each widget

--- frontend/src/components/project-section-form/mock.ts
+++ frontend/src/components/project-section-form/mock.ts
@@ -96,14 +96,14 @@ const education=[{
         "endDate": "present",
         "description": "Cupidatat pariatur ea ex eu voluptate voluptate. Sint consectetur aute eiusmod sit sit reprehenderit et incididunt elit nostrud tempor exercitation. Duis reprehenderit adipisicing veniam minim nisi elit aute minim quis sint ea aute reprehenderit et. Enim velit Lorem proident ad cupidatat. Irure sunt incididunt officia nisi cupidatat nisi officia est. Adipisicing tempor nostrud anim pariatur nulla sit culpa cupidatat duis labore aliquip cupidatat nostrud commodo. Ullamco eu occaecat aute cupidatat irure minim pariatur sint mollit incididunt.",
         "CompanyImage": ""
-        },
-        {
+    },
+    {
             "schoolName": "abcZfwfsfsfsfsfs",
             "degree": "Sde-1",
             "startDate": "3-6-2023",
             "endDate": "present",
             "description": "Cupidatat pariatur ea ex eu voluptate voluptate. Sint consectetur aute eiusmod sit sit reprehenderit et incididunt elit nostrud tempor exercitation. Duis reprehenderit adipisicing veniam minim nisi elit aute minim quis sint ea aute reprehenderit et. Enim velit Lorem proident ad cupidatat. Irure sunt incididunt officia nisi cupidatat nisi officia est. Adipisicing tempor nostrud anim pariatur nulla sit culpa cupidatat duis labore aliquip cupidatat nostrud commodo. Ullamco eu occaecat aute cupidatat irure minim pariatur sint mollit incididunt.",
             "CompanyImage": ""
-            },
+    },
 ]
 export {data,profileDetails,displayPictureWithdummy,skillData,certifications,professionalWorkExperiences}
