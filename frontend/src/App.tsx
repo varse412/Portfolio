d7d59b85aba9412e2173e3a262cb81877f404240
Varun Sehgal <varunsehgal412@gmail.com>
2024-04-07 22:57:52 +0530

    dynamic edit route addition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,27 @@
 import './App.css'
 import {Outlet,Link} from 'react-router-dom'
 import {dashboardScreenName} from '../src/pages/profiles-page/types.ts'
-const dashboardScreens: JSX.Element[] = Object.values(dashboardScreenName).map((screenName, index) => (
-  <div key={index}>
-    <Link to={`profiles/${screenName}`}>{screenName}</Link>
-  </div>
-));
+const dashboardScreens: JSX.Element[] = Object.values(dashboardScreenName).map((screenName, index) => {
+  
+  return (
+   <>
+   {(index!=Object.values(dashboardScreenName).length-1)?(<div key={index} className='flex flex-1 m-2'>
+     <Link to={`profiles/${screenName}`}>{screenName}</Link>
+   </div>): null}
+   </>
+  )
+
+});
+
 function App() {
   return (
    <div id="projectContainer">
      
-     <div className='bg-yellow-300'>1</div>
-     <div className='bg-green-500'>{dashboardScreens}</div>
-     <div className='bg-gray-600'>3</div>
+     <div className='bg-yellow-300 '>1</div>
+     <div className='bg-green-500 flex flex-row flex-wrap justify-between items-center'>{dashboardScreens}</div>
+     <div className='bg-gray-600 flex flex-1 justify-center items-center'>
+     <Link to={`profiles/bio`}>{"bio"}</Link>
+     </div>
      <div className='bg-pink-400'>4</div>
      <div className='flex flex-1'><Outlet/></div>
      <div className='bg-orange-400'>6</div>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,8 @@ import './index.css'
 import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import ErrorPage from './pages/error-page/index.tsx'
 import Profiles from './pages/profiles-page/index.tsx'
+// import EditProject from './pages/projects-page/editProject.tsx'
+import EditContent from './pages/content-edit/index.tsx'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -13,13 +15,40 @@ const router = createBrowserRouter([
     children: [
       { 
         path: "profiles/:profile",
-        element: <Profiles/>
-      }
+        element: <Profiles/>,
+        errorElement: <ErrorPage/>,
+        children: [
+          {
+             path: ":id",
+             element: <EditContent/>,
+             errorElement: <ErrorPage/>
+          }
+        ]
+      },
+     
     ]
   },
+  
+  // EditContent
 
 ])
-
+{/* <EditProject/> */}
+ // {
+      //        path: "profiles/:profile/:id",
+      //        element: <h1>Helo world</h1>,
+      //        errorElement: <ErrorPage/>,
+      // }
+// {
+  //   path: 'profiles/projects/:projectId',
+  //   element: <App/>,
+  //   errorElement: <ErrorPage/>,
+  //   children: [
+  //     { 
+  //       path: "profiles/:profile",
+  //       element: <Profiles/>
+  //     }
+  //   ]
+  // },
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
