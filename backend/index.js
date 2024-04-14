666737283a409592b9d0233bdfd208d5ad0ea0c1
Varun Sehgal <varunsehgal412@gmail.com>
2024-04-14 15:35:08 +0530

    deleted files

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
+import {appRouter} from "../routes/profile.js"
+// require('appRouter') from "./routes/profile"
+// const appRouter= require('../backend/routes/profile')
 dotenv.config()
-
+const app=express()
 const PORT=process.env.port||3000
 
 app.get("/",(req,res)=>{
--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,2 +1,9 @@
 import express from 'express'
 import router from 'express'
+const appRouter=express.Router();
+appRouter.get("prof",(req,res)=>{
+   res.send("from router")
+})
+
+export appRouter;
+// module.exports=appRouter
